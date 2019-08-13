8b491d081e29a865a66ac9f71e4f03296531d858
sera_denoir <angelic.spectra@gmail.com>
2019-08-12 19:29:05 -0700

    Update minecraft_server_start.sh
    Fix errors when not calling from server directory

--- minecraft_server_start.sh
+++ minecraft_server_start.sh
@@ -3,6 +3,7 @@
 #scans the folder for the latest version of forge and adds it to the 'screen' command, launching a minecraft server instance silently
 #change the 'terranova' after the 'screen -dmS' command to a convenient name for you, and the '/opt/minecraft/terranova/' to match the installation point of your server
 
+cd /opt/minecraft/terranova
 app=`ls forge-1.12.2-*.jar | tail -n 1 |grep -i forge`
 #echo "${app}"
-screen -dmS terranova java -server -Xms512M -Xmx4008M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=4 -XX:+AggressiveOpts -jar /opt/minecraft/terranova/${app} nogui
+screen -dmS terranova java -server -Xms512M -Xmx4008M -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=4 -XX:+AggressiveOpts -jar ${app} nogui
